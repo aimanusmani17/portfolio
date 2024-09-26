c7844b6141bcff352c0990a7b9bddf165d1ec03d
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-09-26 17:50:14 +0530

    added responsive design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Styles from '../Styles/Navbar.module.css';
+import logo from '../assets/Aiman_logo.png';
 
 
 
 const Navbar = () => {
     return (
       <nav className={Styles.navbar}>
-        <h2>Aiman Usmani</h2>
+        <a className={Styles.logo} 
+        // onClick={() => scrollToSection('home')}
+        >Aiman Usmani</a>
+        {/* <h2>Aiman Usmani</h2> */}
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
