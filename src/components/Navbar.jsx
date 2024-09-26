b13a948992a546be80e4d26e1be36e8a9a06874e
aimanusmani1700@gmail.com <aimanusmani@gmail.com>
2024-09-26 19:51:24 +0530

    added logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Styles from '../Styles/Navbar.module.css';
-import logo from '../assets/Aiman_logo.png';
+// import logo from '../assets/Aiman_logo.png';
 
 
 
 const Navbar = () => {
     return (
       <nav className={Styles.navbar}>
-        <a className={Styles.logo} 
+        {/* <a className={Styles.logo} 
         // onClick={() => scrollToSection('home')}
-        >Aiman Usmani</a>
-        {/* <h2>Aiman Usmani</h2> */}
+        >Aiman Usmani</a> */}
+        <h2>Aiman Usmani</h2>
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#skills">Skills</a></li>
